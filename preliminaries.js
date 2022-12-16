a351824780250bc51378fd405da10a501d533a57
LeventCelik <celiklevent@pm.me>
2022-12-16 23:17:03 +0300

    Fixes Shamir's bug, implements (untested) BigIntegerAdapter

--- preliminaries.js
+++ preliminaries.js
@@ -1,6 +1,6 @@
 const { group } = require("console")
 
-const DEBUG = true //! Keep this FALSE for release. It prints out secret values.
+const DEBUG = false //! Keep this FALSE for release. It prints out secret values.
 const VERBOSE = false //! Keep this false as well.
 /**
  * The functions accept and return values in base64 string format,
@@ -11,17 +11,23 @@ const VERBOSE = false //! Keep this false as well.
 // --- TESTS ---
 
 function testAll(it=5) {
-	//const groupTest = testGroup(it)
-	//const polTest = testPolynomials(it)
+	let x = new BigIntegerAdapter(1234)
+	let y = new BigIntegerAdapter(4321)
+	print(x.toString())
+	print(x.add(y).toString())
+	print(x.times(y).toString())
+	const groupTest = testGroup(it)
+	const polTest = testPolynomials(it)
 	const shamirTest = testShamir(3, 2, it)
 }
 
 function testPolynomials(it) {
-	//TODO: Time the tests
 	if (DEBUG) print("Testing polynomial operations.")
 	const G = new PrimeGroup()
+	console.time("Polynomial tests")
 	for (let i = 1; i <= it; i++) {
-		const a0 = G.randomElement()
+		console.time("Polynomial test #"+i)
+		const a0 = G.randomExponent()
 		const pol = genPol(a0, 5*i, G)
 		if (VERBOSE) {
 			for (let i = 0; i < pol.length; i++) {
@@ -31,63 +37,73 @@ function testPolynomials(it) {
 			print()
 		}
 		const eval_at_0 = evalPol(pol, 0, G)
-		if (eval_at_0 .eq(a0)) continue
+		console.timeEnd("Polynomial test #"+i)
+		if (a0.eq(eval_at_0)) continue
 		print("Polynomial test failed.")
 		if (DEBUG) {
 			print("Constant term chosen:\n" + a0.toString())
 			print("Constant term evaluated:\n" + eval_at_0.toString())
 		}
+		console.timeEnd("Polynomial tests")
 		return false
 	}
+	console.timeEnd("Polynomial tests")
 	if (DEBUG) print("Polynomial tests successful.")
 	return true
 }
 
 function testShamir(n, t, it=1) {
-	// TODO: Time the tests.
 	const G = new PrimeGroup()
 	if (DEBUG) print("Testing Shamir's Secret Sharing.")
+	console.time("Shamir tests")
 	for (let i = 1; i <= it; i++) {
-		print(i)
-		const secret = G.randomElement()
+		console.time("Shamir test #"+i)
+		const secret = G.randomExponent()
 		const shares = shamirGenShares(secret, n*i, t*i, G)
 		if (VERBOSE) {
 			print("Generated shares:")
 			for (const share of shares)
-			print("\tShare #" + share[0].toString() + ": " + share[1].toString())
+				print("\tShare #" + share[0] + ": " + share[1].toString())
 		}
-		const recons = shamirCombineShares(shares, G)
+		const recons = shamirCombineShares(shares.slice(0, t*i), G)
 		if (VERBOSE) {
 			print("Secret reconstructed:")
 			print("\tOriginal secret: " + secret.toString())
 			print("\tReconstructed secret: " + recons.toString())
 		}
-		if (secret.eq(recons)) continue
+		console.timeEnd("Shamir test #"+i)
+		if (G.expEq(secret, recons)) continue
 		print("Shamir's Secret Sharing tests failed at n=" + n*i + ", t=" + t*i + ".")
 		print("Secret:\n" + secret.toString())
 		print("Recons:\n" + recons.toString())
+		console.timeEnd("Shamir tests")
 		return false
 	}
+	console.timeEnd("Shamir tests")
 	if (DEBUG) print("Shamir's Secret Sharing tests successful.")
 	return true
 }
 
 function testGroup(it) {
-	// TODO: Time the tests.
 	const G = new PrimeGroup()
 	if (DEBUG) print("Testing inversion.")
+	console.time("Group tests")
 	for (let i = 1; i <= it; i++) {
+		console.time("Group test #"+i)
 		let x = G.randomElement()
 		let x_inv = G.inverse(x)
 		let e = G.mul(x, x_inv)
+		console.timeEnd("Group test #"+i)
 		if (e == 1) continue
 		print("Inversion failed. e: " + e.toString())
 		if (VERBOSE) {
 			print("Random x: " + x.toString())
 			print("Calculated x inverse: " + x_inv.toString())
 		}
+		console.timeEnd("Group tests")
 		return false
 	}
+	console.timeEnd("Group tests")
 	if (DEBUG) print("Inversion successful.")
 	return true
 }
@@ -108,15 +124,15 @@ function shamirCombineShares(shares, group) {
 	const n = shares.length
 	let at0 = bigInt(0)
 	for (const point of shares) {
-		const i = point[0]
+		const i = point[0] // int, not bigInt
 		const at_i = point[1]
 		let lambda_i = bigInt(1)
-		for (let j = bigInt(1); j <= n; j++) {
-			if (group.eq(i, j)) continue
-			const temp = group.mul(j, group.inverse(group.subtract(j, i)))
-			lambda_i = group.mul(lambda_i, temp)
+		for (let j = 1; j <= n; j++) {
+			if (i == j) continue
+			const temp = group.expMul(j, bigInt(j-i).modInv(group.order)) // j/j-i
+			lambda_i = group.expMul(lambda_i, temp)
 		}
-		at0 = group.add(at0, group.mul(at_i, lambda_i))
+		at0 = group.expAdd(at0, group.expMul(at_i, lambda_i)) //! Legal only on exponents
 	}
 	return at0
 }
@@ -138,26 +154,27 @@ function shamirGenShares(secret, n, t, group) {
 function genPol(secret, t, group) {
 	/**
 	 * Constructs a degree t-1 polynomial where
-	 * a_0 = secret
+	 * a_0 = secret, needs to be in the exponent range
 	 * a_i = random for all 0<i<t
-	 * All elements are bigIntegers in the given group
+	 * All elements are exponents in the given group represented as bigIntegers
 	 */
+	if (group.order.leq(secret)) //? This really should not be the case. Maybe throw instead?
+		secret = secret.mod(group.order) //? Care about negatives?
 	const pnomial = [secret]
 	for (let i = 1; i < t; i++) {
-		pnomial.push(group.randomElement())
+		pnomial.push(group.randomExponent())
 	}
 	return pnomial
 }
 
 function evalPol(pol, x, group) {
 	/**
-	 * Evaluates the polynomial at x in the given group.
+	 * Evaluates the polynomial at x in the exponents of the given group
 	 */
-	const bigInt = group.bigInt
-	let sum = bigInt(0)
+	let sum = 0
 	for(let i = 0; i < pol.length; i++) {
-		const x_i = group.mul(pol[i], group.pow(x, i))
-		sum = group.add(sum, x_i)
+		const x_i = group.expMul(pol[i], group.expPow(x, i))
+		sum = group.expAdd(sum, x_i)
 	}
 	return sum
 }
@@ -197,7 +214,7 @@ async function oprfMask(secret, group) {
 //--- OPRF end ---
 
 //--- Hash functions ---
-async function hashPrime(str, group) {
+async function hashPrime(str, group) { //* Full domain hash, problem: Z_n*
 	/**
 	 * Hashes a string to an element in the given group.
 	 * Returns a promise resolving to the hash as a hex string.
@@ -234,10 +251,11 @@ async function hash(str) {
 
 class PrimeGroup {
 	// We work on a fixed group with generator 2.
-	// ! This may be problematic for any sort of production code.
+	// ! Hardcoding may be problematic for any sort of production code.
+	// TODO: Reduce the size of this group
 	constructor(modulus=0, generator=0, order=0) {
 		const bigInt = require("big-integer")
-		if(modulus==0) {
+		if(modulus==0) { //* Order 256-bit (or 512-bit)
 			// https://datatracker.ietf.org/doc/rfc3526/ | Group id 15
 			modulus = new bigInt( // prime modulus
 				"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1"+
@@ -272,15 +290,15 @@ class PrimeGroup {
 		 * Returns the inverse of elm in group.
 		 * Employs the fact that elm^order = 1 so elm^(order-1) = elm^(-1)
 		 */
-		return this.pow(this.bigInt(elm), this.subtract(this.order, 1))
+		return this.pow(this.bigInt(elm), this.expSubtract(this.order, 1))
 	}
 
-	expInverse(exp) {
+	expInverse(exp) { //! BUG
 		/**
 		 * Returns the inverse of exponent, so that:
 		 * g^{exp} * g^{exp^-1} = g
 		 */
-		return this.subtract(this.order, this.bigInt(exp))
+		return this.pow(this.bigInt(exp), this.expSubtract(this.order, 2))
 	}
 	
 	randomElement() {
@@ -306,35 +324,133 @@ class PrimeGroup {
 		rnd = Buffer.from(rnd).toString('hex') // Hex
 		return this.bigInt(rnd, 16).mod(this.order) // bigInt
 	}
-
-	mul(x, y) {
-		//* This can likely be made more efficient
+	mul(x, y) { // TODO: Check the efficiency here
 		return this.bigInt(x).times(this.bigInt(y)).mod(this.modulus)
 	}
 
-	add(x, y) {
+	expMul(x, y) { // TODO: Check the efficiency here
+		return this.bigInt(x).times(this.bigInt(y)).mod(this.order)
+	}
+
+	add(x, y) {  //! This group is not additive, be careful
+		if(DEBUG) print("You are adding elements in a multiplicative group, are you sure?")
 		return this.bigInt(x).add(this.bigInt(y)).mod(this.modulus)
 	}
 
-	subtract(x, y) {
-		return this.bigInt(x).subtract(this.bigInt(y)).mod(this.modulus)
+	subtract(x, y) { //! This group is not additive, be careful
+		if(DEBUG) print("You are subtracting elements in a multiplicative group, are you sure?")
+		var cand = this.bigInt(x).subtract(this.bigInt(y)).mod(this.modulus)
+		return cand.leq(0) ? cand.add(this.modulus) : cand
 	}
 
-	pow(x, y) {
+	expAdd(x, y) {
+		return this.bigInt(x).add(this.bigInt(y)).mod(this.order)
+	}
+
+	expSubtract(x, y) {
+		var cand = this.bigInt(x).subtract(this.bigInt(y)).mod(this.order)
+		return cand.leq(0) ? cand.add(this.order) : cand
+	}
+
+	pow(x, y) { // TODO: Check the efficiency here
 		return this.bigInt(x).modPow(this.bigInt(y), this.modulus)
+	}
+
+	expPow(x, y) { // TODO: Check the efficiency here
+		return this.bigInt(x).modPow(this.bigInt(y), this.order)
 	}
 
 	eq(x, y) {
 		// TODO: Check whether elements always map into groups
 		// TODO: Add a map-into-group call
 		const bigInt = this.bigInt
+		// Mod into group
 		var temp_x = bigInt(x).mod(this.modulus)
 		var temp_y = bigInt(y).mod(this.modulus)
-		if (temp_x.leq(0))
-			temp_x = temp_x.add(this.modulus)
-		if (temp_y.leq(0))
-			temp_Y = temp_y.add(this.modulus)
+		// Check positivity
+		temp_x = temp_x.leq(0) ? temp_x.add(this.modulus) : temp_x
+		temp_y = temp_y.leq(0) ? temp_y.add(this.modulus) : temp_y
 		return temp_y.eq(temp_x)
+	}
+
+	expEq(x, y) {
+		// TODO: Check whether elements always map into groups
+		// TODO: Add a map-into-group call
+		const bigInt = this.bigInt
+		// Mod into group
+		var temp_x = bigInt(x).mod(this.order)
+		var temp_y = bigInt(y).mod(this.order)
+		// Check positivity
+		temp_x = temp_x.leq(0) ? temp_x.add(this.order) : temp_x
+		temp_y = temp_y.leq(0) ? temp_y.add(this.order) : temp_y
+		return temp_y.eq(temp_x)
+	}
+}
+
+class BigIntegerAdapter {
+	constructor(value) {
+		this.bigInt = require('big-integer')
+		this.value = this.bigInt(value)
+	}
+	// Base calls
+	add(num) {
+		return new BigIntegerAdapter(this.value.add(num.value))
+	}
+	subtract(num) {
+		return new BigIntegerAdapter(this.value.subtract(num.value))
+	}
+	mul(num) {
+		return new BigIntegerAdapter(this.value.times(num.value))
+	}
+	times(num) {
+		return this.mul(num)
+	}
+	pow(num) {
+		return new BigIntegerAdapter(this.value.pow(num.value))
+	}
+	eq(num) {
+		return this.value.eq(num.value)
+	}
+	leq(num) {
+		return his.value.leq(num.value)
+	}
+	geq(num) {
+		return this.value.geq(num.value)
+	}
+	lesser(num) {
+		return this.value.lesser(num.value)
+	}
+	greater(num) {
+		return this.value.greater(num.value)
+	}
+	// Mod calls, return positive
+	mod(mod) {
+		var result = new BigIntegerAdapter(this.value.mod(mod))
+		return result.lesser(0) ? result.add(mod) : result
+	}
+	addMod(num, mod) {
+		return this.add(num).mod(mod)
+	}
+	subtractMod(num, mod) {
+		return this.subtract(num).mod(mod)
+	}
+	mulMod(num, mod) {
+		return this.mul(num).mod(mod)
+	}
+	powMod(num, mod) {
+		return this.pow(num).mod(mod)
+	}
+	eqMod(num, mod) {
+		x = this.mod(mod)
+		y = num.mod(mod)
+		return x.eq(y)
+	}
+	invMod(mod) {
+		return new BigIntegerAdapter(this.value.modInv(mod))
+	}
+	// Other functionalities
+	toString() {
+		return this.value.toString()
 	}
 }
 
